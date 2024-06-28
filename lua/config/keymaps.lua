73f02c529c56834e3ff036303dc7ce5f5e52cad4
timwenzeltech <48419301+timwenzeltech@users.noreply.github.com>
2024-06-28 16:33:13 +0200

    Toggle Term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,18 +2,17 @@ vim.g.mapleader = " "
 --UndoTree
 vim.keymap.set("n", "<leader>u", vim.cmd.UndoTreeToggle, { desc = "Toggle UndoTree" })
 -- Move to window using the <ctrl> hjkl keys
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+vim.keymap.set({ "n" }, "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set({ "n" }, "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set({ "n" }, "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set({ "n" }, "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 -- Resize window using <ctrl> arrow keys
-vim.keymap.set("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-vim.keymap.set("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-vim.keymap.set("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-vim.keymap.set("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
---vim.keymap.set("n", "<leader>pv", vim.cmd.NvimTreeToggle)
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "NetRW me daddy" })
+vim.keymap.set({ "t", "n" }, "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set({ "t", "n" }, "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+vim.keymap.set({ "t", "n" }, "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+vim.keymap.set({ "t", "n" }, "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
 -- Jump up and down stay centered
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Jump down" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Jump up, Jump up, " })
@@ -30,3 +29,8 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "<C-c>", ":lua MiniBufremove.delete()<cr>", { desc = "Delete Buffer" })
+--Terminal Navigation
+vim.keymap.set("t", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("t", "<C-j>", "<C-w>j", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("t", "<C-k>", "<C-w>k", { desc = "Go to Left Window", remap = true })
+vim.keymap.set("t", "<C-l>", "<C-w>j", { desc = "Go to Left Window", remap = true })
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
 			require("tokyonight").setup({
 				-- your configuration comes here
 				-- or leave it empty to use the default settings
-				style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+				style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
 				transparent = true, -- Enable this to disable setting the background color
 				terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
 				styles = {
@@ -18,7 +18,7 @@ return {
 					floats = "transparent", -- style for floating windows
 				},
 			})
-			--vim.cmd("colorscheme tokyonight")
+			-- vim.cmd("colorscheme tokyonight")
 		end,
 	},
 	{
@@ -26,7 +26,7 @@ return {
 		name = "rose-pine",
 		config = function()
 			require("rose-pine").setup({
-				variant = "auto", -- auto, main, moon, or dawn
+				variant = "moon", -- auto, main, moon, or dawn
 				dark_variant = "main", -- main, moon, or dawn
 				dim_inactive_windows = false,
 				extend_background_behind_borders = true,
@@ -44,6 +44,51 @@ return {
 				},
 			})
 			vim.cmd("colorscheme rose-pine")
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		config = function()
+			require("onedark").setup({
+				-- Main options --
+				style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				transparent = true, -- Show/hide background
+				term_colors = true, -- Change terminal color as per the selected theme style
+				ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+				cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+				-- toggle theme style ---
+				toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+				toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" }, -- List of styles to toggle between
+
+				-- Change code style ---
+				-- Options are italic, bold, underline, none
+				-- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+				code_style = {
+					comments = "none",
+					keywords = "none",
+					functions = "none",
+					strings = "none",
+					variables = "none",
+				},
+
+				-- Lualine options --
+				lualine = {
+					transparent = false, -- lualine center bar transparency
+				},
+
+				-- Custom Highlights --
+				colors = {}, -- Override default colors
+				highlights = {}, -- Override highlight groups
+
+				-- Plugins Config --
+				diagnostics = {
+					darker = true, -- darker colors for diagnostic
+					undercurl = false, -- use undercurl instead of underline for diagnostics
+					background = true, -- use background color for virtual text
+				},
+			})
+			-- vim.cmd("colorscheme onedark")
 		end,
 	},
 }
