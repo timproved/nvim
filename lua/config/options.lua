6f2f6eecbbdca3a6bafd8eaabf58e95d92460fcf
timwenzeltech <48419301+timwenzeltech@users.noreply.github.com>
2024-07-24 18:04:14 +0200

    updates

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,19 +1,19 @@
 vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
 
-vim.opt.number = true         -- display line numbersawdawdawdadw
+vim.opt.number = true -- display line numbersawdawdawdadw
 vim.opt.relativenumber = true -- display relative line numbers
-vim.opt.numberwidth = 2       -- set width of line number column
-vim.opt.signcolumn = "yes"    -- always show sign column
-vim.opt.wrap = false          -- display lines as single line
-vim.opt.scrolloff = 15        -- number of lines to keep above/below curso
-vim.opt.sidescrolloff = 8     -- number of columns to keep to the left/right of cursor
+vim.opt.numberwidth = 2 -- set width of line number column
+vim.opt.signcolumn = "yes" -- always show sign column
+vim.opt.wrap = false -- display lines as single line
+vim.opt.scrolloff = 15 -- number of lines to keep above/below curso
+vim.opt.sidescrolloff = 8 -- number of columns to keep to the left/right of cursor
 
-vim.opt.expandtab = true      -- convert tabs to spaces
-vim.opt.shiftwidth = 4        -- number of spaces inserted for each indentation level
-vim.opt.tabstop = 4           -- number of spaces inserted for tab character
-vim.opt.softtabstop = 4       -- number of spaces inserted for <Tab> key
-vim.opt.smartindent = true    -- enable smart indentation
-vim.opt.breakindent = true    -- enable line breaking indentation
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.shiftwidth = 4 -- number of spaces inserted for each indentation level
+vim.opt.tabstop = 4 -- number of spaces inserted for tab character
+vim.opt.softtabstop = 4 -- number of spaces inserted for <Tab> key
+vim.opt.smartindent = true -- enable smart indentation
+vim.opt.breakindent = true -- enable line breaking indentation
 vim.opt.smartindent = true
 vim.opt.conceallevel = 1
 
@@ -31,13 +31,13 @@ vim.opt.guicursor = ""
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
-
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "120"
 vim.opt.laststatus = 3
 
 -- Searching Behaviors
 vim.opt.ignorecase = true -- ignore case in search
-vim.opt.smartcase = true  -- match case if explicitly stated
+vim.opt.smartcase = true -- match case if explicitly stated
 
 vim.opt.pumheight = 20
+vim.opt.fillchars = { eob = " " }
--- lua/plugins/util.lua
+++ lua/plugins/util.lua
@@ -1,107 +1,121 @@
 return {
-	-- Markdown Preview
-	{
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+    -- Markdown Preview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 
-	-- Mini Utils
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
+    -- Mini Utils
+    {
+        "echasnovski/mini.nvim",
+        config = function()
+            -- Better Around/Inside textobjects
+            --
+            -- Examples:
+            --  - va)  - [V]isually select [A]round [)]paren
+            --  - yinq - [Y]ank [I]nside [N]ext [']quote
+            --  - ci'  - [C]hange [I]nside [']quote
+            require("mini.ai").setup({ n_lines = 500 })
 
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require("mini.surround").setup()
+            -- Add/delete/replace surroundings (brackets, quotes, etc.)
+            --
+            -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+            -- - sd'   - [S]urround [D]elete [']quotes
+            -- - sr)'  - [S]urround [R]eplace [)] [']
+            require("mini.surround").setup()
 
-			require("mini.comment").setup()
+            require("mini.comment").setup()
 
-			require("mini.bufremove").setup()
-		end,
-	},
+            require("mini.bufremove").setup()
+            local statusline = require("mini.statusline")
+            statusline.setup({
+                use_icons = true,
+                laststatus = 3,
+                set_vim_settings = false,
+            })
 
-	-- Vim Tmux Navigator
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
+            -- You can configure sections in the statusline by overriding their
+            -- default behavior. For example, here we set the section for
+            -- cursor location to LINE:COLUMN
+            ---@diagnostic disable-next-line: duplicate-set-field
+            statusline.section_location = function()
+                return "%2l:%-2v"
+            end
+        end,
+    },
 
-	--Python Venv Selector
-	{
-		"linux-cultist/venv-selector.nvim",
-		dependencies = {
-			"neovim/nvim-lspconfig",
-			"mfussenegger/nvim-dap",
-			"mfussenegger/nvim-dap-python", --optional
-			{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-		},
-		lazy = "VeryLazy",
-		branch = "regexp", -- This is the regexp branch, use this for the new version
-		config = function()
-			require("venv-selector").setup({
-				name = { "venv", ".venv", "env", ".env" },
-				auto_refresh = false,
-				dap_enabled = true,
-			})
-		end,
-		keys = {
-			-- Keymap to open VenvSelector to pick a venv.
-			{ "<leader>vs", "<cmd>VenvSelect<cr>" },
-			-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-			{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
-		},
-	},
+    -- Vim Tmux Navigator
+    {
+        "christoomey/vim-tmux-navigator",
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+        },
+        keys = {
+            { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
+    },
 
-	-- ToggleTerm
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			require("toggleterm").setup({
-				open_mapping = [[<C-t>]],
-				close_on_exit = false,
-				shell = vim.o.shell,
-			})
-			function _G.set_terminal_keymaps()
-				local opts = { buffer = 0 }
-				vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-				vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-				vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-				vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-				vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-				vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-				vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-			end
+    --Python Venv Selector
+    {
+        "linux-cultist/venv-selector.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "mfussenegger/nvim-dap",
+            "mfussenegger/nvim-dap-python", --optional
+            { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+        },
+        lazy = "VeryLazy",
+        branch = "regexp", -- This is the regexp branch, use this for the new version
+        config = function()
+            require("venv-selector").setup({
+                name = { "venv", ".venv", "env", ".env" },
+                auto_refresh = false,
+                dap_enabled = true,
+            })
+        end,
+        keys = {
+            -- Keymap to open VenvSelector to pick a venv.
+            { "<leader>vs", "<cmd>VenvSelect<cr>" },
+            -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+            { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+        },
+    },
 
-			-- if you only want these mappings for toggle term use term://*toggleterm#* instead
-			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
-		end,
-	},
+    -- ToggleTerm
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping = [[<C-t>]],
+                close_on_exit = false,
+                shell = vim.o.shell,
+            })
+            function _G.set_terminal_keymaps()
+                local opts = { buffer = 0 }
+                vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+                vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+                vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+                vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+                vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+                vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+                vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+            end
+
+            -- if you only want these mappings for toggle term use term://*toggleterm#* instead
+            vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+        end,
+    },
 }
