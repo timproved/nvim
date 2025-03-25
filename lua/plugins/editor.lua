d40329062d61b011535cd73558c858d272a6f941
Tim Wenzel <48419301+timproved@users.noreply.github.com>
2025-03-25 10:13:47 +0100

    Harpoon baby

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -127,9 +127,53 @@ return {
 		---@type oil.SetupOpts
 		opts = {},
 		-- Optional dependencies
-		dependencies = { { "echasnovski/mini.icons", opts = {} } },
-		dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-		-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+		dependencies = {
+			{
+				"echasnovski/mini.icons",
+				opts = {},
+			},
+			"nvim-tree/nvim-web-devicons",
+		},
 		lazy = false,
+	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local harpoon = require("harpoon")
+
+			-- REQUIRED
+			harpoon:setup()
+			-- REQUIRED
+
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon:list():add()
+			end)
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
+			end)
+
+			vim.keymap.set("n", "<C-1>", function()
+				harpoon:list():select(1)
+			end)
+			vim.keymap.set("n", "<C-2>", function()
+				harpoon:list():select(2)
+			end)
+			vim.keymap.set("n", "<C-3>", function()
+				harpoon:list():select(3)
+			end)
+			vim.keymap.set("n", "<C-4>", function()
+				harpoon:list():select(4)
+			end)
+
+			-- Toggle previous & next buffers stored within Harpoon list
+			vim.keymap.set("n", "<C-S-P>", function()
+				harpoon:list():prev()
+			end)
+			vim.keymap.set("n", "<C-S-N>", function()
+				harpoon:list():next()
+			end)
+		end,
 	},
 }
