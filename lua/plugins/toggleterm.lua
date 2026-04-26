2ad1661967bb12321bd64e86e46e7f0fb2b5ebc2
timproved <48419301+timproved@users.noreply.github.com>
2026-04-26 14:35:17 +0200

    fix(term): Added ctrl escape

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,5 +5,23 @@ vim.pack.add({
 require("toggleterm").setup({
   cmd = { "ToggleTerm", "TermExec" },
   open_mapping = [[<C-t>]],
-  size = 20,
+  size = 15,
+})
+
+local group = vim.api.nvim_create_augroup("toggleterm-keymaps", { clear = true })
+local function set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  -- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  -- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = group,
+  pattern = "term://*",
+  callback = set_terminal_keymaps,
 })
